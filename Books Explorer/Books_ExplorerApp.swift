882df8f23767f22f4e0e1a13a2c8670895d1fbe4
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-10-20 12:42:44 +0330

    Initial Commit

---
+++ Books Explorer/Books_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_ExplorerApp.swift
+//  Books Explorer
+//
+//  Created by Hamid on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Books_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
